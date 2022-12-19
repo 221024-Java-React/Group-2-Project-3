0e1aa766b3e6ac87a92fc8f0efca86f50abf3ea1
He1i0s-OS <61101765+He1i0s-OS@users.noreply.github.com>
2022-12-19 12:00:16 -0800

    Added Login

---
+++ frontend/banking-application/src/Components/Home/Home.tsx
@@ -0,0 +1,12 @@
+import Navigation from "../Navigation/Navigation";
+
+const Home = () => {
+
+	return (
+		<>
+			<Navigation />
+		</>
+	);
+};
+
+export default Home;
---
+++ frontend/banking-application/src/Components/Login/Login.tsx
@@ -0,0 +1,59 @@
+import { useState, useContext } from "react";
+import { AuthContext } from "../../Context/AuthContext";
+
+import Navigation from "../Navigation/Navigation";
+
+import "./Login.css";
+
+const Login = () => {
+
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const { login } = useContext(AuthContext);
+
+    const emailHandler = (event: any) => {
+        setEmail(event.target.value);
+    };
+
+    const passwordHandler = (event: any) => {
+        setPassword(event.target.value);
+    };
+
+    const loginHandler = (event: any) => {
+        event.preventDefault();
+
+        login(email, password);
+    };
+
+    return (
+        <>
+            <Navigation />
+            <div className="login">
+                <h1>Login</h1>
+                <form onSubmit={loginHandler}>
+                    <input
+                        type="text"
+                        name="email"
+                        value={email}
+                        placeholder="Email"
+                        onChange={emailHandler}
+                    />
+                    <input
+                        type="password"
+                        name="password"
+                        value={password}
+                        placeholder="Password"
+                        onChange={passwordHandler}
+                    />
+                    <button type="submit">Login</button>
+                </form>
+                <div className="member">
+                    <h4>Don't Have An Account?</h4>
+                    <a href="/register">Sign Up</a>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Login;
