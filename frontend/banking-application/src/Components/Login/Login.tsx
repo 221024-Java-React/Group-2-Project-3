28c321831b9936e108f25b235c2fd9ac15b66b2d
He1i0s-OS <61101765+He1i0s-OS@users.noreply.github.com>
2022-12-21 11:28:20 -0800

    Updated Styling

--- frontend/banking-application/src/Components/Login/Login.tsx
+++ frontend/banking-application/src/Components/Login/Login.tsx
@@ -8,55 +8,62 @@ import "./Login.css";
 
 const Login = () => {
 
-    const [email, setEmail] = useState<string>("");
-    const [password, setPassword] = useState<string>("");
-    const { login } = useContext(AuthContext);
+	const [email, setEmail] = useState<string>("");
+	const [password, setPassword] = useState<string>("");
+	const { login } = useContext(AuthContext);
 
-    const emailHandler = (event: any) => {
-        setEmail(event.target.value);
-    };
+	const emailHandler = (event: any) => {
+		setEmail(event.target.value);
+	};
 
-    const passwordHandler = (event: any) => {
-        setPassword(event.target.value);
-    };
+	const passwordHandler = (event: any) => {
+		setPassword(event.target.value);
+	};
 
-    const loginHandler = (event: any) => {
-        event.preventDefault();
+	const loginHandler = (event: any) => {
+		event.preventDefault();
 
-        login(email, password);
-    };
+		login(email, password);
+	};
 
-    return (
-        <>
-            <Navigation />
-            <div className="content">
-                <div className="login box">
-                    <h2>Login</h2>
-                    <form className="form" onSubmit={loginHandler}>
-                        <input
-                            type="text"
-                            name="email"
-                            value={email}
-                            placeholder="Email"
-                            onChange={emailHandler}
-                        />
-                        <input
-                            type="password"
-                            name="password"
-                            value={password}
-                            placeholder="Password"
-                            onChange={passwordHandler}
-                        />
-                        <button className="login-button" type="submit">Login</button>
-                    </form>
-                    <h3 className="member">Don't Have An Account?</h3>
-                    <div className="form">
-                        <Link className="login-button" to="/register">Create An Account</Link>
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+	return (
+		<>
+			<div className="page">
+				<Navigation />
+				<div className="content">
+					<div className="login box">
+						<h2>Login</h2>
+						<form className="form" onSubmit={loginHandler}>
+							<input
+								type="text"
+								name="email"
+								value={email}
+								placeholder="Email"
+								onChange={emailHandler}
+							/>
+							<input
+								type="password"
+								name="password"
+								value={password}
+								placeholder="Password"
+								onChange={passwordHandler}
+							/>
+							<button className="login-button" type="submit">Login</button>
+						</form>
+						<h3 className="member">Don't Have An Account?</h3>
+						<div className="form">
+							<Link className="login-button" to="/register">Create An Account</Link>
+						</div>
+					</div>
+					<div className="advertisement">
+						<h2>Sign Up Today</h2>
+						<p>Sign up today to create your Checkings Savings & Loan Accounts!</p>
+					</div>
+				</div>
+				<div className="background"></div>
+			</div>
+		</>
+	);
 };
 
 export default Login;
