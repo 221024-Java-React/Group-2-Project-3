644735bec3193172f9edbe4df24de3ce44d84c59
He1i0s-OS <61101765+He1i0s-OS@users.noreply.github.com>
2022-12-28 14:50:20 -0800

    Updated Styling

--- frontend/banking-application/src/Components/Footer/Footer.tsx
+++ frontend/banking-application/src/Components/Footer/Footer.tsx
@@ -10,6 +10,8 @@ const Footer = () => {
                 <Link to="/help" className="footer-flex-item">Help</Link>
                 <Link to="/faqs" className="footer-flex-item">FAQs</Link>
                 <Link to="/about" className="footer-flex-item">About Us</Link>
+                <Link to="/retrieve" className="footer-flex-item">Forgot Username</Link>
+                <Link to="/reset" className="footer-flex-item">Forgot Password</Link>
             </div>
 		</div>
 	);
