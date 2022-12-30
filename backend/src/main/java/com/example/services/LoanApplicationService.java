2f7bba766a4e3f51f6c3d630c5bf799393e850c4
RafaelCaso <94573618+RafaelCaso@users.noreply.github.com>
2022-12-30 11:58:05 -0500

    backend boilerplate for loanApp

--- backend/src/main/java/com/example/services/LoanApplicationService.java
+++ backend/src/main/java/com/example/services/LoanApplicationService.java
@@ -1,2 +1,29 @@
-package com.example.services;public class LoanApplicationService {
+package com.example.services;
+
+import com.example.models.LoanApplication;
+import com.example.repository.LoanApplicationRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+@AllArgsConstructor
+public class LoanApplicationService {
+
+    @Autowired
+    private LoanApplicationRepository loanRepo;
+
+    public LoanApplication create(LoanApplication loanApp) {
+        return loanRepo.save(loanApp);
+    }
+
+    public Boolean approve(Integer id) {
+        // use AccountService to add funds to user account
+        // return true so front end can give some feedback
+
+        return true;
+    }
 }
