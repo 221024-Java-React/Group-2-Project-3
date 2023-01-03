36e47502e53059f1db2a0647a28bc9ef9c926a5d
He1i0s-OS <61101765+He1i0s-OS@users.noreply.github.com>
2023-01-03 07:57:01 -0800

    Updated Styling

--- frontend/banking-application/src/Components/Help/Help.tsx
+++ frontend/banking-application/src/Components/Help/Help.tsx
@@ -11,11 +11,8 @@ const Help = () => {
 			<Background />
             <Navigation />
             <div className="content">
-				<h2>Help</h2>
-			</div>
-			<div className="flex-container">
-                <div className="flex-item">
-                    <p className="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                <h2>Help</h2>
+                <p className="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                         sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
                         Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
@@ -28,8 +25,7 @@ const Help = () => {
                         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
                         Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
                         Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                </div>
-            </div>
+			</div>
 			<Footer />
 		</div>
 	);
