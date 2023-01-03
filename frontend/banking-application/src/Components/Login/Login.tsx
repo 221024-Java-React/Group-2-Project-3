738b73f6f5daaea6fc24bc2bf9eaa1043ccfc536
He1i0s-OS <61101765+He1i0s-OS@users.noreply.github.com>
2023-01-03 08:49:57 -0800

    Updated Advertisement

--- frontend/banking-application/src/Components/Login/Login.tsx
+++ frontend/banking-application/src/Components/Login/Login.tsx
@@ -10,80 +10,73 @@ import Navigation from "../Navigation/Navigation";
 import "./Login.css";
 
 const Login = () => {
-  const [validForm, setValidForm] = useState<boolean>(true);
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
-  const { login, loggedInUser } = useContext(AuthContext);
+	const [validForm, setValidForm] = useState<boolean>(true);
+	const [email, setEmail] = useState<string>("");
+	const [password, setPassword] = useState<string>("");
+	const { login, loggedInUser } = useContext(AuthContext);
 
-  const passwordHandler = (event: any) => {
-    setPassword(event.target.value);
-  };
+	const passwordHandler = (event: any) => {
+		setPassword(event.target.value);
+	};
 
-  const emailHandler = (event: any) => {
-    setEmail(event.target.value);
-  };
+	const emailHandler = (event: any) => {
+		setEmail(event.target.value);
+	};
 
-  const loginHandler = (event: any) => {
-    login(email, password).then(success => {
-        if (success)
-            setValidForm(true);
-        else
-            setValidForm(false)
-    });
-  };
+	const loginHandler = (event: any) => {
+		login(email, password).then(success => {
+			if (success)
+				setValidForm(true);
+			else
+				setValidForm(false)
+		});
+	};
 
-  return (
-    <>
-      <div className="page">
-        <Background />
-        <Navigation />
-        <div className="content">
-          <div className="login box">
-            <h2>Login</h2>
-            <form className="form">
-              {!validForm && (
-                <p className="invalid">Incorrect Email or Password</p>
-              )}
-              <input
-                type="text"
-                name="email"
-                value={email}
-                placeholder="Email"
-                onChange={emailHandler}
-              />
-              <input
-                type="password"
-                name="password"
-                value={password}
-                placeholder="Password"
-                onChange={passwordHandler}
-              />
-              <Link className="login-button" to="/" onClick={loginHandler}>
-                Login
-              </Link>
-            </form>
-            {/* <h3 className="member">Forgot Your Username?</h3>
-
-						<div className="form">
-							<Link className="login-button" to="/retrieve">Retrieve Username</Link>
+	return (
+		<>
+			<div className="page">
+				<Background />
+				<Navigation />
+				<div className="flex-container">
+				<div className="login box">
+							<h2>Login</h2>
+							<form className="form">
+								{!validForm && (
+									<p className="invalid">Incorrect Email or Password</p>
+								)}
+								<input
+									type="text"
+									name="email"
+									value={email}
+									placeholder="Email"
+									onChange={emailHandler}
+								/>
+								<input
+									type="password"
+									name="password"
+									value={password}
+									placeholder="Password"
+									onChange={passwordHandler}
+								/>
+								<Link className="login-button" to="/" onClick={loginHandler}>
+									Login
+								</Link>
+							</form>
+							<h3 className="member">Don't Have An Account?</h3>
+							<div className="form">
+								<Link className="login-button" to="/register">
+									Create An Account
+								</Link>
+							</div>
 						</div>
-						<h3 className="member">Forgot Your Password?</h3>
-						<div className="form">
-							<Link className="login-button" to="/reset">Reset Password</Link>
-						</div> */}
-            <h3 className="member">Don't Have An Account?</h3>
-            <div className="form">
-              <Link className="login-button" to="/register">
-                Create An Account
-              </Link>
-            </div>
-          </div>
-          {/* <Advertisement /> */}
-        </div>
-        <Footer />
-      </div>
-    </>
-  );
+					<div className="flex-item">
+						<Advertisement />
+					</div>
+				</div>
+				<Footer />
+			</div>
+		</>
+	);
 };
 
 export default Login;
